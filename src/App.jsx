638b247e762690032c300322b81ff67d63fc8d6b
Eduardo5011 <eduardo.rosado5011@gmail.com>
2025-01-24 18:37:47 -0500

    created page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from "./pages";
 
 const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
-}
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
 
-export default App
+export default App;
