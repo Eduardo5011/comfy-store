0f21a7b305ef942404fd8976c2dd5a6cc0ccb678
Eduardo5011 <eduardo.rosado5011@gmail.com>
2025-02-19 20:10:46 -0500

    created navLink component and updated navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
+import NavLinks from "./NavLinks";
 
 const Navbar = () => {
   return (
@@ -22,14 +23,14 @@ const Navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
             >
-              Nav Links
+              <NavLinks/>
             </ul>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal">nav links</ul>
+          <ul className="menu menu-horizontal"><NavLinks/></ul>
         </div>
         <div className="navbar-end">
           {/* CART ICON */}
