1706accbb2cf781a7add40cc542351865e7a5b6c
Eduardo5011 <eduardo.rosado5011@gmail.com>
2025-02-23 19:42:31 -0500

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import NavLinks from "./NavLinks";
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(false);
+  const handleTheme = () => {
+    setTheme(!theme);
+  };
+
   return (
     <nav className="bg-base-200">
       <div className="navbar align-element">
@@ -25,25 +30,31 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-200 rounded-box w-52"
             >
-              <NavLinks/>
+              <NavLinks />
             </ul>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal"><NavLinks/></ul>
+          <ul className="menu menu-horizontal">
+            <NavLinks />
+          </ul>
         </div>
         <div className="navbar-end">
           {/* CART ICON */}
+          <label className="swap swap-rotate">
+            <input type="checkbox" onChange={handleTheme} />
+            {/* sun icon */}
+            <BsSunFill className="swap-on h-4 w-3" />
+            {/* moon icon */}
+            <BsMoonFill className="swap-off h-4 w-3" />
+          </label>
           {/* CART LINK */}
-          <NavLink
-            to="/cart"
-            className="btn btn-ghost btn-circle btn-md ml-4"
-          >
+          <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
             <div className="indicator">
-                <BsCart3 className="h-6 w-6" />
-                <span className="badge badge-sm badge-primary indicator-item">
+              <BsCart3 className="h-6 w-6" />
+              <span className="badge badge-sm badge-primary indicator-item">
                 0
-                </span>
+              </span>
             </div>
           </NavLink>
         </div>
