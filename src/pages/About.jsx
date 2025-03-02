4f05bd6a2fea3041b14fd1140f08c2040c07fbe9
Eduardo5011 <eduardo.rosado5011@gmail.com>
2025-03-01 22:15:07 -0500

    updated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <h1 className="text-4xl">About</h1>
-  )
-}
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, ipsa!
+        Corporis veniam cupiditate doloribus eos maxime inventore quasi, magni
+        eaque esse eum? Amet necessitatibus praesentium, minus aut fugiat
+        architecto consectetur. Id nulla earum a, repudiandae eius animi
+        dignissimos explicabo saepe?
+      </p>
+    </>
+  );
+};
 
-export default About
+export default About;
