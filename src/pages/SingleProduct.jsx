fcead349bbbc528a78db7fe45a713b5179c9601d
Eduardo5011 <eduardo.rosado5011@gmail.com>
2025-03-27 20:51:14 -0400

    updated singleProduct component

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { useLoaderData } from 'react-router-dom';
-import { formatPrice, customFetch } from '../utils';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { formatPrice, customFetch } from "../utils";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
-export const loader = async() => {
-  return null
-}
+export const loader = async ({ params }) => {
+  const response = await customFetch(`/products/${params.id}`);
+  return { product: response.data.data };
+};
 
 const SingleProduct = () => {
-  return (
-    <h1 className="text-4xl">SingleProduct</h1>
-  )
-}
+  const { product } = useLoaderData();
+  const { image, title, price, description, colors, company } =
+    product.attributes;
+  const dollarsAmount = formatPrice(price);
 
-export default SingleProduct
+  return <h1 className="text-4xl">SingleProduct</h1>;
+};
+
+export default SingleProduct;
