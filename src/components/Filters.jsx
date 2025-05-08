fc12d454ea1caaf8852deafa8b10ececb0db03d7
Eduardo5011 <eduardo.rosado5011@gmail.com>
2025-05-08 19:59:29 -0400

    updated filter, formSelect and FormInput

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import { Link, useLoaderData, Form } from "react-router-dom";
+import FormInput from "./FormInput";
+import FormSelect from "./FormSelect";
 
 const Filters = () => {
+  const { meta } = useLoaderData();
   return (
-    <div>Filters</div>
-  )
-}
+    <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+      {/* SEARCH */}
+      <FormInput
+        type="search"
+        label="search product"
+        name="search"
+        size="input-sm"
+      />
+      {/* CATEGORIES */}
+      <FormSelect
+        label="select category"
+        name="category"
+        list={meta.categories}
+        size="select-sm"
+      />
+      {/* COMPANIES */}
+      <FormSelect
+        label="select company"
+        name="company"
+        list={meta.companies}
+        size="select-sm"
+      />
+      {/* ORDER */}
+      <FormSelect
+        label="sort by"
+        name="order"
+        list={["a-z", "z-a", "high", "low"]}
+        size="select-sm"
+      />
+      {/* BUTTONS */}
+      <button type="submit" className="btn btn-primary btn-sm">
+        Search
+      </button>
+      <Link to="/products" className="btn btn-accent btn-sm">
+        reset
+      </Link>
+    </Form>
+  );
+};
 
-export default Filters
+export default Filters;
