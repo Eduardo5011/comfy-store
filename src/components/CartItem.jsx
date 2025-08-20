acee7a027057727d2c5cb0eaf23b6aefff2a7b63
Eduardo5011 <eduardo.rosado5011@gmail.com>
2025-08-19 21:09:10 -0400

    updated cartItemsList and CartItem

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { formatPrice, generateAmountOptions } from "../utils";
+import { removeItem, editItem } from "../features/cart/cartSlice";
+import { useDispatch } from "react-redux";
 
-const CartItem = () => {
-  return (
-    <div className='text-3xl'>CartItem</div>
-  )
-}
+const CartItem = ({ cartItem }) => {
+  const { cartID, title, price, image, amount, company, productColor } =
+    cartItem;
 
-export default CartItem
+  return <article className="mb-12 flex flex-col gap-y-4 sm:flex-row flex-wrap border-b border-base-300 pb-6 last:border-b-0">
+
+<img src={image} alt={title} classNme="" />
+  </article>
+};
+
+export default CartItem;
