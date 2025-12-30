54c88b44e9fa38ce6882bf0c0814f0da36b3e409
Eduardo5011 <eduardo.rosado5011@gmail.com>
2025-12-29 21:04:46 -0500

    updated

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -7,7 +7,7 @@ import { toast } from "react-toastify";
 import { clearCart } from "../features/cart/cartSlice";
 
 export const action =
-  (store) =>
+  (store, queryClient) =>
   async ({ request }) => {
     const formData = await request.formData();
     const { name, address } = Object.fromEntries(formData);
@@ -34,17 +34,17 @@ export const action =
           },
         }
       );
+      queryClient.removeQueries(["orders"]);
       store.dispatch(clearCart());
       toast.success("Order placed successfully");
       return redirect("/orders");
     } catch (error) {
-      console.log(error);
       const errorMessage =
         error?.response?.data?.error?.message ||
         "there was an error placing your order";
       toast.error(errorMessage);
       // if (error.response.status === 401 || error.response.status === 403) return redirect("/login"); // NOTE: this is not the best practice, but it works for now
-      if (error.response.status === 401 || 403) return redirect("/login"); 
+      if (error?.response?.status === 401 || 403) return redirect("/login"); 
       return null;
     }
   };
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -2,10 +2,29 @@ import React from "react";
 import { redirect, useLoaderData } from "react-router-dom";
 import { toast } from "react-toastify";
 import { customFetch } from "../utils";
-import { OrdersList, ComplexPaginationContainer, SectionTitle } from "../components";
+import {
+  OrdersList,
+  ComplexPaginationContainer,
+  SectionTitle,
+} from "../components";
+
+const ordersQuery = (params, user) => {
+  return {
+    queryKey: [
+      "orders",
+      user.username,
+      params.page ? parseInt(params.page) : 1,
+    ],
+    queryFn: () =>
+      customFetch("/orders", {
+        params,
+        headers: { Authorization: `Bearer ${user.token}` },
+      }),
+  };
+};
 
 export const loader =
-  (store) =>
+  (store, queryClient) =>
   async ({ request }) => {
     const user = store.getState().userState.user;
 
@@ -17,20 +36,16 @@ export const loader =
       ...new URL(request.url).searchParams.entries(),
     ]);
     try {
-      const response = await customFetch("/orders", {
-        params,
-        headers: { Authorization: `Bearer ${user.token}` },
-      });
-      console.log(response);
+      const response = await queryClient.ensureQueryData(
+        ordersQuery(params, user)
+      );
       return { orders: response.data.data, meta: response.data.meta };
     } catch (error) {
-      console.log(error);
       const errorMessage =
         error?.response?.data?.error?.message ||
         "there was an error placing your order";
       toast.error(errorMessage);
-      // if (error.response.status === 401 || error.response.status === 403) return redirect("/login"); // NOTE: this is not the best practice, but it works for now
-      if (error.response.status === 401 || 403) return redirect("/login");
+      if (error?.response?.status === 401 || 403) return redirect("/login");
       return null;
     }
   };
@@ -40,11 +55,13 @@ const Orders = () => {
   if (meta.pagination.total < 1) {
     return <SectionTitle text="please make an order" />;
   }
-  return <>
-    <SectionTitle text="Your Orders" />
-    <OrdersList />
-    <ComplexPaginationContainer  />
-  </>
+  return (
+    <>
+      <SectionTitle text="Your Orders" />
+      <OrdersList />
+      <ComplexPaginationContainer />
+    </>
+  );
 };
 
 export default Orders;
